d05803bb88b81590cb0abac06e0ece36622770a9
yoshifan <yoshifan@users.noreply.github.com>
2015-05-19 18:37:09 -0700

    - Add Hitbox. - Add an option to not produce any tweets when just starting the script. - Don't crash when a stream request fails for some reason (e.g. Twitch being down). - Support printing non-ASCII to Windows command line. - Use classes, and no longer use globals.

--- main.py
+++ main.py
@@ -1,84 +1,108 @@
-import argparse
 import datetime
 import time
 
 import requests    # Must get this from PyPI
 
 import config
+import windows_cmd_support
 
 
-    
-recently_live_expire_interval = datetime.timedelta(
-    minutes=config.recently_live_expire_minutes
-)
-games_by_twitch_name = dict(
-    [(g['twitch'], g) for g in config.games if ('twitch' in g)]
-)
-streamers_by_twitch_name = dict(
-    [(s['twitch'].lower(), s) for s in config.streamers if ('twitch' in s)]
-)
 
-twitch_recently_live = {}
+class StreamRequestException(Exception):
+    pass
 
 
 
 def debug_print(s, required_verbosity):
     if config.verbosity >= required_verbosity:
         print(s)
+        
+        
+        
+class Site(object):
+    
+    def __init__(self, site_name, channel_link_format):
+        
+        self.channel_link_format = channel_link_format
+        
+        self.games_by_onsite_name = dict(
+            [(g[site_name], g) for g in config.games
+            if (site_name in g)]
+        )
+        self.streamers_by_onsite_name = dict(
+            [(s[site_name].lower(), s) for s in config.streamers
+            if (site_name in s)]
+        )
+        self.recently_live = dict()
+        self.first_check = True
 
-
-
-def update_twitch():
-    
-    time_now = datetime.datetime.now()
-    
-    for channel_name, d in twitch_recently_live.iteritems():
-        if time_now - d['last_seen_live'] > recently_live_expire_interval:
-            twitch_recently_live.pop(name)
-    
-    
-    # Get channels on the Twitch team, with various info including live status
-    url = (
-        'http://api.twitch.tv/api/team/'
-        + config.twitch_team
-        + '/all_channels.json'
-    )
-    response = requests.get(url)
-    response_json = response.json()
-    
-    for d in response_json['channels']:
+    def check_streams(self):
+                
+        first_check = self.first_check
+        # Be sure to set this before any possible return statement.
+        self.first_check = False
         
-        ch = d['channel']
-        is_live = ch['status'] == 'live'
+        time_now = datetime.datetime.utcnow()
+        recently_live_expire_interval = datetime.timedelta(
+            minutes=config.recently_live_expire_minutes
+        )
         
-        if not is_live:
-            # Live streams are listed first, so once we see a non-live
-            # stream, we can stop.
-            break
+        # Age the recently-live entries, and remove any that are too old
+        for channel_name, d in self.recently_live.iteritems():
+            if time_now - d['last_seen_live'] > recently_live_expire_interval:
+                self.recently_live.pop(name)
+                
+        try:
+            stream_dicts = self.request_streams()
+        except StreamRequestException:
+            # Web request failed (and we were able to catch it).
+            # Just try again at the next polling interval.
+            return
         
-        channel_name = ch['name'].lower()
-        game_name = ch['meta_game']
-        is_playing_tracked_game = game_name in games_by_twitch_name
-        
-        if not is_playing_tracked_game:
-            debug_print(ch['display_name'] + " is playing other stuff", 2)
-            continue
+        for d in stream_dicts:
             
-        just_went_live = channel_name not in twitch_recently_live
-        just_started_game = False
-        if not just_went_live:
-            game_last_seen = twitch_recently_live[channel_name]['game']
-            just_started_game = game_name != game_last_seen
-        
-        if just_went_live or just_started_game:
+            channel_name = d['channel_name']
+            channel_site_display = d['channel_site_display']
+            game_name = d['game_name']
             
-            # We'll announce this streamer.
+            is_playing_tracked_game = game_name in self.games_by_onsite_name
             
-            game_d = games_by_twitch_name[game_name]
+            if not is_playing_tracked_game:
+                debug_print(
+                    channel_site_display + " is playing other stuff", 2
+                )
+                continue
+                
+            just_went_live = channel_name not in self.recently_live
+            just_started_game = False
+            
+            if not just_went_live:
+                game_last_seen = self.recently_live[channel_name]['game_name']
+                just_started_game = (game_name != game_last_seen)
+                    
+            # Update recently-live status.
+            self.recently_live[channel_name] = dict(
+                last_seen_live = time_now,
+                game_name = game_name,
+            )
+            
+            if (not just_went_live) and (not just_started_game):
+                debug_print(
+                    channel_site_display + " is still playing " + game_name, 2
+                )
+                continue
+                
+            if first_check and not config.tweet_streams_seen_on_startup:
+                debug_print(
+                    channel_site_display + " is playing " + game_name, 2
+                )
+                continue
+                
+            # Everything looks good; we'll announce this streamer.
             
             # Figure out how to display the streamer's name.
-            if channel_name in streamers_by_twitch_name:
-                streamer_d = streamers_by_twitch_name[channel_name]
+            if channel_name in self.streamers_by_onsite_name:
+                streamer_d = self.streamers_by_onsite_name[channel_name]
                 if 'twitter' in streamer_d:
                     # First choice: Twitter handle, if any.
                     streamer_display = '@' + streamer_d['twitter']
@@ -86,32 +110,127 @@ def update_twitch():
                     # Second choice: a specified display name, if any.
                     streamer_display = streamer_d['display_name']
             else:
-                # Third choice: just use the Twitch display name.
-                streamer_display = ch['display_name']
+                # Third choice: just use the site's display name.
+                # Only difference from channel_name is possible capitalization.
+                streamer_display = channel_site_display
                 
+            game_d = self.games_by_onsite_name[game_name]
+            
             announce_text = \
                 "{game} {streamer} is live: {link}".format(
                     game = game_d['display_name'],
                     streamer = streamer_display,
-                    link = 'twitch.tv/' + channel_name
+                    link = self.channel_link_format.format(
+                        channel_name=channel_name
+                    )
                 )
-            debug_print(announce_text.encode('utf-8'), 1)
+            debug_print(announce_text, 1)
+            
+            
+            
+class Twitch(Site):
+    
+    def __init__(self):
         
-        twitch_recently_live[channel_name] = dict(
-            last_seen_live = time_now,
-            game = game_name,
+        super(Twitch, self).__init__('twitch', 'twitch.tv/{channel_name}')
+        
+    def request_streams(self):
+        
+        url = (
+            'http://api.twitch.tv/api/team/'
+            + config.twitch_team
+            + '/all_channels.json'
         )
-        debug_print(ch['display_name'] + " is playing " + game_name, 2)
         
-    debug_print(time_now, 2)
+        try:
+            response = requests.get(url)
+        except requests.exceptions.RequestException as e:
+            debug_print("[ERROR on Twitch requests.get() call] " + e.strerror, 1)
+            raise StreamRequestException
             
+        try:
+            response_json = response.json()
+        except ValueError as e:
+            debug_print("[ERROR on Twitch json() call] " + e.strerror, 1)
+            raise StreamRequestException
+        
+        stream_dicts = []
+        
+        # Iterate over the team's channels.
+        for obj in response_json['channels']:
+            ch = obj['channel']
+            is_live = (ch['status'] == 'live')
             
+            if not is_live:
+                # Live streams are listed first, so once we see a non-live
+                # stream, we can stop.
+                break
+                
+            stream_dicts.append(dict(
+                channel_name = ch['name'].lower(),
+                channel_site_display = ch['display_name'],
+                game_name = ch['meta_game'],
+            ))
+        
+        return stream_dicts
+        
+        
             
+class Hitbox(Site):
+    
+    def __init__(self):
+        
+        super(Hitbox, self).__init__('hitbox', 'hitbox.tv/{channel_name}')
+        
+    def request_streams(self):
+    
+        url = (
+            'http://api.hitbox.tv/team/'
+            + config.hitbox_team
+            + '?media=true&media_type=live&liveonly=true'
+        )
+        
+        try:
+            response = requests.get(url)
+        except requests.exceptions.RequestException as e:
+            debug_print("[ERROR on Hitbox requests.get() call] " + e.strerror, 1)
+            raise StreamRequestException
+            
+        try:
+            response_json = response.json()
+        except ValueError as e:
+            debug_print("[ERROR on Hitbox json() call] " + e.strerror, 1)
+            raise StreamRequestException
+        
+        stream_dicts = []
+        
+        # Iterate over the team's currently-live streams.
+        for live_obj in response_json['media']['livestream']:
+            stream_dicts.append(dict(
+                channel_name = live_obj['media_name'].lower(),
+                channel_site_display = live_obj['media_display_name'],
+                game_name = live_obj['category_name'],
+            ))
+        
+        return stream_dicts
+
+
+
 
 if __name__ == '__main__':
     
+    if config.support_windows_command_line:
+        windows_cmd_support.add_support()
+    
+    twitch = Twitch()
+    hitbox = Hitbox()
+    
     while True:
-        update_twitch()
+        twitch.check_streams()
+        hitbox.check_streams()
+        
+        debug_print(datetime.datetime.now(), 2)
+        debug_print('', 1)  # Empty line
         time.sleep(config.sleep_seconds)
     
     
